0016a302add7d519637bd7aea08cb804417bd068
Ali Rostami <rostamiev@gmail.com>
2019-01-25 20:47:26 +0100

    small change

--- python/python-code-samples/data-cleaning.py
+++ python/python-code-samples/data-cleaning.py
@@ -6,4 +6,5 @@ df_clean = df.copy()
 df_clean['Animal'] = df_clean['Animal'].str[2:]
 df_clean['Body weight (kg)'] = df_clean['Body weight (kg)'].str.replace('!', '.')
 df_clean['Brain weight (g)'] = df_clean['Brain weight (g)'].str.replace('!', '.')
-df_clean.head()
+df_clean.info()
+print(df_clean.head())
