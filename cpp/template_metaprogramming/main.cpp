92b067562816fa26947fc33f14c25303eab113e4
Ali Rostami <rostamiev@gmail.com>
2019-11-20 13:59:17 +0100

    if constexp

--- cpp/template_metaprogramming/main.cpp
+++ cpp/template_metaprogramming/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <memory>
 #include "mybinary.h"
+
+template <typename T>
+auto get_value(T t) {
+    if constexpr (std::is_pointer_v<T>)
+        return *t;
+    else
+        return t;
+}
+
+template<int  N>
+constexpr int fibonacci() {return fibonacci<N-1>() + fibonacci<N-2>(); }
+template<>
+constexpr int fibonacci<1>() { return 1; }
+template<>
+constexpr int fibonacci<0>() { return 0; }
+
+template<int N>
+constexpr int fibonacci2()
+{
+    if constexpr (N>=2)
+        return fibonacci2<N-1>() + fibonacci2<N-2>();
+    else
+        return N;
+}
+
 
 int main() {
     unsigned const one = mybinary<1>::value;
@@ -9,5 +35,15 @@ int main() {
     std::cout << one << std::endl;
     std::cout << three << std::endl;
     std::cout << five << std::endl;
+    std::cout << seven << std::endl;
+
+    auto pi = std::make_unique<int>(9);
+    int i = 9;
+
+    std::cout << get_value(pi.get()) << "\n";
+    std::cout << get_value(i) << "\n";
+
+    std::cout << fibonacci<10>() << std::endl;
+    std::cout << fibonacci2<10>() << std::endl;
     return 0;
 }
