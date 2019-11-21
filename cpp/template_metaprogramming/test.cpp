3928ab89e3a40788b6ee1ef3f915584a20529015
Ali Rostami <rostamiev@gmail.com>
2019-11-21 20:53:50 +0100

    differentiation with template

--- cpp/template_metaprogramming/test.cpp
+++ cpp/template_metaprogramming/test.cpp
@@ -11,6 +11,32 @@ constexpr int fibonacci2()
         return N;
 }
 
+class psc_f {
+public:
+    psc_f(double alpha) : alpha(alpha)
+
+    double operator()(double x) const {
+        return sin(alpha * x) + cos(x);
+    }
+
+private:
+    double alpha;
+};
+
+template <typename F, typename T>
+class derivative {
+public:
+    derivative(const F &f, const T &t) : f(f), t(t) {}
+
+    T operator()(const T &x) const {
+        return (f(x + h) - f(x)) / h;
+    }
+
+private:
+    const F &f;
+    T t;
+};
+
 int main() {
     fibonacci2<10>();
 }
