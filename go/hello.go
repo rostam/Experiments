af1731dcad41b7e2015e4d86e19dea5373f2c33b
Ali Rostami <rostamiev@gmail.com>
2019-11-24 18:21:00 +0100

    map in go

--- go/hello.go
+++ go/hello.go
@@ -6,4 +6,50 @@ import "fmt"
 
 func main() {
     fmt.Println("Hello World")
+    elements := map[string]map[string]string{
+    "H": map[string]string{
+      "name":"Hydrogen",
+      "state":"gas",
+    },
+    "He": map[string]string{
+      "name":"Helium",
+      "state":"gas",
+    },
+    "Li": map[string]string{
+      "name":"Lithium",
+      "state":"solid",
+    },
+    "Be": map[string]string{
+      "name":"Beryllium",
+      "state":"solid",
+    },
+    "B":  map[string]string{
+      "name":"Boron",
+      "state":"solid",
+    },
+    "C":  map[string]string{
+      "name":"Carbon",
+      "state":"solid",
+    },
+    "N":  map[string]string{
+      "name":"Nitrogen",
+      "state":"gas",
+    },
+    "O":  map[string]string{
+      "name":"Oxygen",
+      "state":"gas",
+    },
+    "F":  map[string]string{
+      "name":"Fluorine",
+      "state":"gas",
+    },
+    "Ne":  map[string]string{
+      "name":"Neon",
+      "state":"gas",
+    },
+  }
+
+  if el, ok := elements["Li"]; ok {
+    fmt.Println(el["name"], el["state"])
+  }
 }
