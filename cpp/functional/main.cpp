d5a025516fbb231b3731a0c41d72ffa708657cb1
Ali Rostami <rostamiev@gmail.com>
2019-12-08 13:35:33 +0100

    last version of functional read lines of files

--- cpp/functional/main.cpp
+++ cpp/functional/main.cpp
@@ -2,20 +2,22 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+//#include <ranges>
 
-int count_lines(const std::string& filename)
-{
+int count_lines(const std::string& filename) {
     std::ifstream in(filename);
-    return std::count(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>(in), '\n');
+    return std::count(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>(), '\n');
 }
 
-std::vector<int> count_lines_in_files(const std::vector<std::string> files)
-{
-    return files | transform(count_lines)
+std::vector<int> count_lines_in_files(const std::vector<std::string>& files) {
+    std::vector<int> results(files.size());
+    std::transform(files.cbegin(), files.cend(), results.begin(), count_lines);
+    return results;
 }
 
 int main() {
-    auto res = count_lines_in_files(std::vector<int>({'file.txt','file2.txt'}));
+    std::vector<std::string> files = {"file1.txt", "file2.txt"};
+    auto res = count_lines_in_files(files);
     std::cout << res[0] << " " << res[1] << std::endl;
     return 0;
 }
