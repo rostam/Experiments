186ea045b66bc71ef3df3ffb368e51467b063168
Ali Rostami <rostamiev@gmail.com>
2019-12-08 13:07:24 +0100

    counting lines in file in a functional way

--- cpp/functional/main.cpp
+++ cpp/functional/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+int count_lines(const std::string& filename)
+{
+    std::ifstream in(filename);
+    return std::count(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>(in), '\n');
+}
+
+std::vector<int> count_lines_in_files(const std::vector<std::string> files)
+{
+    return files | transform(count_lines)
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto res = count_lines_in_files(std::vector<int>({'file.txt','file2.txt'}));
+    std::cout << res[0] << " " << res[1] << std::endl;
     return 0;
 }
