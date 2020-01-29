93569bd130525599644514a79a624b2efd338bd4
Ali Rostami <rostamiev@gmail.com>
2020-01-29 15:17:40 +0100

    function ptr?

--- cpp/cpp-new-features/cpp20.cpp
+++ cpp/cpp-new-features/cpp20.cpp
@@ -8,12 +8,23 @@ using namespace std;
 
 extern "C" {
 
-static int my_callback(int a)
-{
+static int my_callback(int a) {
     return a + 1;
 }
 
 }
+
+template <typename> struct fnptr;
+template <typename Ret, typename... Args> struct fnptr<Ret(Args...)> {
+    void *obj = nullptr;
+    Ret (*wrap)(void*, Args...) = nullptr;
+
+    fnptr() = default;
+    fnptr(Ret target(Args...)) : fnptr([=] (Args... args) { return target(args...); }) {}
+    template <typename T> fnptr(T&& fn) : obj(&fn), wrap([](void *fn, Args... args) { return (*(T *)fn)(args...); }) {}
+
+    Ret operator()(Args... args) const { return wrap(obj, args...); }
+};
 
 int main() {
     cerr << my_callback(1);
